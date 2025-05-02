d8b05e727dbab00c449c8da3a2577f7e1463b4b9
Emmanuel <opensoftweb@gmail.com>
2025-05-02 22:48:53 +0100

    test: bearish

--- test/FonzoMarket.t.sol
+++ test/FonzoMarket.t.sol
@@ -31,10 +31,10 @@ contract FonzoMarketTest is Test {
         ftsoV2 = new MockFtsoV2();
         fonzo = new FonzoMarket(owner, address(ftsoV2));
 
+        vm.warp(25 minutes);
+
         updateFlarePriceFeed(1);
         fonzo.initializeMarket(FLR_USD_ID);
-
-        vm.warp(25 minutes);
     }
 
     function test_constructor() public view {
@@ -62,5 +62,42 @@ contract FonzoMarketTest is Test {
 
         vm.expectRevert(IFonzoMarket.MarketAlreadyExist.selector);
         fonzo.initializeMarket(BTC_USD_ID);
+    }
+
+    function test_bearish_succeeds() public {
+        bytes32 positionId = keccak256(abi.encodePacked(FLR_USD_ID, uint256(2), userA));
+
+        vm.prank(userA);
+        vm.expectEmit();
+        emit IFonzoMarket.Predicted(FLR_USD_ID, 2, userA, positionId, IFonzoMarket.Option.DOWN, 2 ether);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+    }
+
+    function test_bearish_reverts_when_market_does_not_exist() public {
+        vm.expectRevert(IFonzoMarket.MarketNotInitialized.selector);
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(BTC_USD_ID, 1);
+    }
+
+    function test_bearish_reverts_when_entry_not_allowed() public {
+        vm.warp(block.timestamp + 5 minutes + 20);
+        vm.expectRevert(IFonzoMarket.EntryClosed.selector);
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 1);
+    }
+
+    function test_bearish_reverts_if_no_stake() public {
+        vm.prank(userA);
+        vm.expectRevert(IFonzoMarket.AmountCannotBeZero.selector);
+        fonzo.bearish(FLR_USD_ID, 2);
+    }
+
+    function test_bearish_reverts_when_position_exist() public {
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+
+        vm.prank(userA);
+        vm.expectRevert(IFonzoMarket.PositionExist.selector);
+        fonzo.bearish{value: 3 ether}(FLR_USD_ID, 2);
     }
 }
