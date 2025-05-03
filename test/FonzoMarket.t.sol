ee69934ffc405375acb4d85cef24d9d4cdf2c789
Emmanuel <opensoftweb@gmail.com>
2025-05-03 10:34:12 +0100

    test: settle

--- test/FonzoMarket.t.sol
+++ test/FonzoMarket.t.sol
@@ -137,4 +137,120 @@ contract FonzoMarketTest is Test {
         vm.expectRevert(IFonzoMarket.PositionExist.selector);
         fonzo.bullish{value: 3 ether}(FLR_USD_ID, 2);
     }
+
+    function test_settle_succeed() public {
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+
+        vm.prank(userB);
+        fonzo.bullish{value: 4 ether}(FLR_USD_ID, 2);
+
+        vm.warp(block.timestamp + 5 minutes);
+        uint256 userBBalanceAfter = userB.balance;
+
+        updateFlarePriceFeed(1);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 1);
+
+        vm.warp(block.timestamp + 5 minutes + 5);
+        updateFlarePriceFeed(2);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 2);
+
+        bytes32 positionId = keccak256(abi.encodePacked(FLR_USD_ID, uint256(2), userB));
+        uint256 expectedReward = 5.8 ether;
+        uint256[] memory roundIds = new uint256[](1);
+        roundIds[0] = 2;
+
+        vm.expectEmit();
+        emit IFonzoMarket.Claim(FLR_USD_ID, 2, userB, positionId, expectedReward);
+        vm.prank(userB);
+        fonzo.settle(FLR_USD_ID, roundIds);
+
+        vm.expectRevert(IFonzoMarket.NoReward.selector);
+        vm.prank(userA);
+        fonzo.settle(FLR_USD_ID, roundIds);
+
+        assertEq(userB.balance, userBBalanceAfter + expectedReward);
+    }
+
+    function test_settle_reverts_when_round_not_resolved() public {
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+
+        vm.prank(userB);
+        fonzo.bullish{value: 3 ether}(FLR_USD_ID, 2);
+
+        vm.warp(block.timestamp + 5 minutes);
+        updateFlarePriceFeed(1);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 1);
+
+        uint256[] memory roundIds = new uint256[](1);
+        roundIds[0] = 2;
+
+        vm.prank(userA);
+        vm.expectRevert(IFonzoMarket.NoReward.selector);
+        fonzo.settle(FLR_USD_ID, roundIds);
+    }
+
+    function test_settle_reverts_when_user_has_no_position() public {
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+
+        vm.prank(userB);
+        fonzo.bullish{value: 4 ether}(FLR_USD_ID, 2);
+
+        vm.warp(block.timestamp + 5 minutes);
+        updateFlarePriceFeed(1);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 1);
+
+        vm.warp(block.timestamp + 5 minutes + 5);
+        updateFlarePriceFeed(2);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 2);
+
+        uint256[] memory roundIds = new uint256[](1);
+        roundIds[0] = 2;
+
+        vm.expectRevert(IFonzoMarket.PositionNotFound.selector);
+        vm.prank(userC);
+        fonzo.settle(FLR_USD_ID, roundIds);
+    }
+
+    function test_settle_reverts_when_trying_to_settle_position_twice() public {
+        vm.prank(userA);
+        fonzo.bearish{value: 2 ether}(FLR_USD_ID, 2);
+
+        vm.prank(userB);
+        fonzo.bullish{value: 5 ether}(FLR_USD_ID, 2);
+
+        vm.warp(block.timestamp + 5 minutes);
+        updateFlarePriceFeed(1);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 1);
+
+        vm.warp(block.timestamp + 5 minutes + 5);
+        updateFlarePriceFeed(2);
+
+        vm.prank(userC);
+        fonzo.resolve{value: 0.5 ether}(FLR_USD_ID, 2);
+
+        uint256[] memory roundIds = new uint256[](1);
+        roundIds[0] = 2;
+
+        vm.prank(userB);
+        fonzo.settle(FLR_USD_ID, roundIds);
+
+        vm.prank(userB);
+        vm.expectRevert(IFonzoMarket.Claimed.selector);
+        fonzo.settle(FLR_USD_ID, roundIds);
+    }
 }
